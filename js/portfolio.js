1189befd7853e58b326ea7c8bd775b106341ee4a
Nate Mallison <healthynate@gmail.com>
2018-03-02 15:22:51 -0500

    Got basic modal setup for email, added body !important css to prevent scroll bar jump when modal is opened

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,42 @@
 $(document).ready(function(){
   $('.parallax').parallax();
+
+  $(document).on('click', 'a[href^="#"]', function(event){
+    event.preventDefault();
+  
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+     }, 800);
+  });
+
+  $('#floating_hamburger').on('click', function(event){
+    //const icon = $('#hamburger-icon').html();
+
+    // if (icon === 'menu') {
+    //   $('#hamburger-icon').html('close');
+    // } else {
+    //   $('#hamburger-icon').html('menu');
+    // }
+    const clicked = $('#hamburger-icon').hasClass('clicked');
+
+    if (clicked) {
+      $('#hamburger-icon').removeClass('clicked');
+      $('#hamburger-icon').addClass('unclicked');
+      $(this).one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
+        function(event) {
+          $('#hamburger-icon').html('menu');
+      });
+    } else {
+      $('#hamburger-icon').removeClass('unclicked');
+      $('#hamburger-icon').addClass('clicked');
+      $(this).one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
+        function(event) {
+          $('#hamburger-icon').html('close');
+      }); 
+    }
+  });
+
+  $('.modal').modal();
 });
 
-$(document).on('click', 'a[href^="#"]', function (event) {
-  event.preventDefault();
-
-  $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-   }, 800);
-});
-
-// $("document").ready(function(){
-//   $(".modal-trigger").leanModal();
-// });
 
