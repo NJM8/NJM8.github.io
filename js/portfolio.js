5eaf89ee06b43a4cfffdaba8fbefd47569374da1
Nate Mallison <healthynate@gmail.com>
2018-03-07 15:03:59 -0500

    optimizations after pageSpeed and lightSpeed audits. Optimized image sizes, added rel="noopener" to external links, removed local materialize library and added minified cdn, updated jQuery cdn, added simple js/css lazy loader for images

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
   $('.parallax').parallax();
 
+  [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = function() {
+      img.removeAttribute('data-src');
+    };
+  });
+
   $(document).on('click', 'a[href^="#"]', function(event){
     event.preventDefault();
   
