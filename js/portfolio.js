56bfea10cab4469b00d42d33f5252529c20c2759
Nate Mallison <healthynate@gmail.com>
2018-04-09 08:03:31 -0400

    typo in js comments

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  // simple lazy loader for images, switchws data-src attribute to src 
+  // simple lazy loader for images, switches data-src attribute to src 
   [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
     img.setAttribute('src', img.getAttribute('data-src'));
     img.onload = function() {
@@ -10,7 +10,6 @@ $(document).ready(function(){
   // simple scrolling animator. when clicking a # link it will get the offset to the linked element and animate there
   $(document).on('click', 'a[href^="#"]', function(event){
     event.preventDefault();
-  
     $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
      }, 800);
